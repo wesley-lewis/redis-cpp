7d42afc94a640cf038543312c22b3ac6560dcacc
wesley-lewis <lewis.wes.23@gmail.com>
2023-08-02 20:06:55 +0530

    get, set, del function added

--- server.cpp
+++ server.cpp
@@ -11,6 +11,50 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include <vector>
+#include <string>
+
+const uint32_t k_max_args = 4096;
+
+enum
+{
+  RES_OK = 0,
+  RES_ERR = 1,
+  RES_NX = 2,
+};
+
+static std::map<std::string, std::string> g_map;
+
+static uint32_t do_get(
+    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+  if (!g_map.count(cmd[1]))
+  {
+    return RES_NX;
+  }
+  std::string &val = g_map[cmd[1]];
+  assert(val.size() <= k_max_msg);
+  memcpy(res, val.data(), val.size());
+  *reslen = (uint32_t)val.size();
+  return RES_OK;
+}
+
+static uint32_t do_set(
+    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+  (void)res;
+  (void)reslen;
+  g_map[cmd[1]] = cmd[2];
+  return RES_OK;
+}
+
+static uint32_t do_del(
+    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+  (void)res;
+  (void)reslen;
+  g_map.erase(cmd[1]);
+  return RES_OK;
+}
 
 static void msg(const char *msg)
 {
@@ -107,6 +151,44 @@ static int32_t accept_new_conn(std::vector<Conn *> &fd2conn, int fd)
 
 static void state_req(Conn *conn);
 static void state_res(Conn *conn);
+
+static int32_t parse_req(
+    const uint8_t *data, size_t len, std::vector<std::string> &out)
+{
+  if (len < 4)
+  {
+    return -1;
+  }
+  uint32_t n = 0;
+  memcpy(&n, &data[0], 4);
+  if (n > k_max_args)
+  {
+    return -1;
+  }
+  size_t pos = 4;
+  while (n != 0)
+  {
+    if (pos + 4 > len)
+    {
+      return -1;
+    }
+    uint32_t sz = 0;
+    memcpy(&sz, &data[pos], 4);
+    if (pos + 4 + sz > len)
+    {
+      return -1;
+    }
+    out.push_back(std::string((char *)&data[pos + 4], sz));
+    pos += 4 + sz;
+    --n;
+  }
+
+  if (pos != len)
+  {
+    return -1; // extra data
+  }
+  return 0;
+}
 
 static int32_t do_request(
     const uint8_t *req, uint32_t reqlen,
